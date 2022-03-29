9d3a76123208b25ebcd240fa3ee1ee368c7e5d03
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2022-03-29 17:03:03 -0600

    Add dog package

--- install_networking_utilities.fish
+++ install_networking_utilities.fish
@@ -5,6 +5,7 @@ function install_networking_utilities -d 'Install networking utilities'
     set -la pkglist traceroute
     set -la pkglist nmap
     set -la pkglist nss
+    set -la pkglist dog
 
     for pkg in pkglist
         __check_if_installed $pkg
