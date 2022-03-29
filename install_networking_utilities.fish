ad317e57cb9182a34f643db9fe71a0ebecd46bb5
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2022-03-29 14:34:05 -0600

    First commit

---
+++ install_networking_utilities.fish
@@ -0,0 +1,12 @@
+function install_networking_utilities -d 'Install networking utilities'
+    set -la pkglist gping
+    set -la pkglist mtr
+    set -la pkglist prettyping
+    set -la pkglist traceroute
+    set -la pkglist nmap
+    set -la pkglist nss
+
+    for pkg in pkglist
+        __check_if_installed $pkg
+
+end
